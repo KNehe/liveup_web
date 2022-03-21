22c2669b38d0022e8b6dd0c787b1004436191fb8
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-03-21 11:38:50 +0300

    change styles, replace html/img with next/image

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ import Link from "next/link";
 import { useSelector } from "react-redux";
 
 import styles from "../styles/Home.module.css";
+import PrescribeSVG from "../public/images/doctors_in_white.svg";
+import ManualSVG from "../public/images/hittingwall.svg";
+import ReferSVG from "../public/images/doctors_in_white.svg";
+import PrescribeScreenshot from "../public/images/prescribe_screenshot.PNG";
 
 export default function Home() {
   const { authDetails } = useSelector((state) => state.auth);
@@ -34,7 +38,13 @@ export default function Home() {
             <Link href="/login">
               <a>{authDetails?.user ? "Continue working" : "Start working"}</a>
             </Link>
-            <a href="https://youtu.be/FrIVVXfFy-M" target='_blank' rel="noreferrer">Watch Demo</a>
+            <a
+              href="https://youtu.be/FrIVVXfFy-M"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Watch Demo
+            </a>
           </div>
         </div>
       </section>
@@ -47,25 +57,29 @@ export default function Home() {
             <h2> Track Prescriptions</h2>
             <p>
               Record prescriptions for your patients every day. Be able to
-              lookup a patient&apos;s past prescription by you or any other doctor.
-              This saves you the headache of looking up past prescriptions from
-              books which might have been misplaced or destroyed
+              lookup a patient&apos;s past prescription by you or any other
+              doctor. This saves you the headache of looking up past
+              prescriptions from books which might have been misplaced or
+              destroyed
             </p>
           </div>
-          <img
-            src="/images/progress_tracking.svg"
-            alt="An illustration of prescription tracking"
-            className={styles.feature_img}
-          />
+          <div className={styles.feature_img}>
+            <Image
+              src={PrescribeSVG}
+              alt="An illustration of prescription tracking"
+              objectFit="contain"
+            />
+          </div>
         </section>
 
         {/* avoid manual processes */}
         <section className={styles.feature}>
-          <img
-            src="/images/hittingwall.svg"
-            alt="An illustration of prescription tracking"
-            className={styles.feature_img}
-          />
+          <div className={styles.feature_img}>
+            <Image
+              src={ManualSVG}
+              alt="An illustration of prescription tracking"
+            />
+          </div>
           <div className={styles.text}>
             <h2>Avoid manual processes</h2>
             <p>
@@ -90,88 +104,96 @@ export default function Home() {
               particular patient can easily be viewed.
             </p>
           </div>
-          <img
-            src="/images/doctors_in_white.svg"
-            alt="An illustration of prescription tracking"
-            className={styles.feature_img}
-          />
+          <div className={styles.feature_img}>
+            <Image
+              src={ReferSVG}
+              alt="An illustration of prescription tracking"
+            />
+          </div>
         </section>
       </section>
 
       {/* About */}
 
       <section className={styles.About} id="about">
-      <h2 className={styles.title}>About</h2>
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.about_wrapper}>
+          <div className={styles.about_text}>
+            <p className={styles.description}>
+              LiveUp helps doctors to track patient prescriptions, avoid
+              redundancy and manual processes. I was inspired to create this
+              project after I remembered the long processes and repetitive steps
+              at a dispensary in my town. I was asked to provide information
+              about past medications I received but could not give. This was
+              because the book that the health worker had written in was
+              destroyed by water. I also noticed some patients carried more than
+              one book to every office they entered. If one or all got misplaced
+              or destroyed, alot of information can be lost. This would require
+              the health worker to record the details a second or N times.
+            </p>
+            <p>
+              The source code of the{"  "}
+              <a
+                href="http://github.com/knehe/liveup_api/"
+                title="link to api's source code"
+              >
+                {"  "} API {"  "}
+              </a>
+              and{" "}
+              <a
+                href="https://github.com/KNehe/liveup_web"
+                title="link to front end's source code"
+              >
+                this site
+              </a>{" "}
+              are available on GitHub.
+            </p>
 
-      <img
-          src="/images/prescribe_screenshot.PNG"
-          alt="a screenshot of modal where a doctor can record a prescription"
-        />
-          <p className={styles.description}>
-            LiveUp helps doctors to track patient prescriptions, avoid
-            redundancy and manual processes. I was inspired to create this
-            project after I remembered the long processes and repetitive steps
-            at a dispensary in my town. I was asked to provide information about
-            past medications I received but could not give. This was because the
-            book that the health worker had written in was destroyed by water. I
-            also noticed some patients carried more than one book to every
-            office they entered. If one or all got misplaced or destroyed, alot
-            of information can be lost. This would require the health worker to
-            record the details a second or N times.
-          </p>
-          <p>
-            The source code of the{"  "}
-            <a
-              href="http://github.com/knehe/liveup_api/"
-              title="link to api's source code"
-            >
-              {"  "} API {"  "}
-            </a>
-            and{" "}
-            <a
-              href="https://github.com/KNehe/liveup_web"
-              title="link to front end's source code"
-            >
-              this site
-            </a>{" "}
-            are available on GitHub.
-          </p>
+            <p>Find me on the following sites:</p>
+            <ul>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/nehemiah-kamolu/"
+                  title="link to my LinkedIn profile"
+                >
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://twitter.com/Nehemiah_Nehe"
+                  title="link to my twitter profile"
+                >
+                  Twitter
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/knehe/"
+                  title="link to my github profile"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://medium.com/@Nehe_Kamolu"
+                  title="link to my medium articles"
+                >
+                  Medium
+                </a>
+              </li>
+            </ul>
+          </div>
 
-          <p>Find me on the following sites:</p>
-          <ul>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/nehemiah-kamolu/"
-                title="link to my LinkedIn profile"
-              >
-                LinkedIn
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://twitter.com/Nehemiah_Nehe"
-                title="link to my twitter profile"
-              >
-                Twitter
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/knehe/"
-                title="link to my github profile"
-              >
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://medium.com/@Nehe_Kamolu"
-                title="link to my medium articles"
-              >
-                Medium
-              </a>
-            </li>
-          </ul>
+          <div className={styles.about_img}>
+            <Image
+              src={PrescribeScreenshot}
+              objectFit="contain"
+              alt="a screenshot of modal where a doctor can record a prescription"
+            />
+          </div>
+        </div>
       </section>
     </div>
   );
